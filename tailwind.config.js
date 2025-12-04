df0414913bfcea83b0e5e35412e630dc91110d47
Dimar Tarmizi <dimartarmizi@gmail.com>
2025-12-05 00:17:14 +0700

    Add custom font family 'Inter' to Tailwind configuration and apply it in app.css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,16 @@
 /** @type {import('tailwindcss').Config} */
+import defaultTheme from 'tailwindcss/defaultTheme'
+
 export default {
 	content: [
 		'./resources/**/*.{blade.php,js,vue,php}', // semua file di resources & subfolder
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				sans: ['Inter', ...defaultTheme.fontFamily.sans],
+			},
+		},
 	},
 	plugins: [],
 }
