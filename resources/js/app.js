2601c4ee6fda044b0cfcb7372cecc7fb711b4799
Dimar Tarmizi <dimartarmizi@gmail.com>
2025-12-04 12:14:44 +0700

    Add NProgress for page loading indicators and update dependencies

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,5 +1,8 @@
 import { createApp, h } from 'vue';
 import { createInertiaApp } from '@inertiajs/inertia-vue3';
+import { Inertia } from '@inertiajs/inertia';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
 
 createInertiaApp({
     resolve: async name => {
@@ -12,8 +15,15 @@ createInertiaApp({
         return module.default;
     },
     setup({ el, App, props, plugin }) {
-        createApp({ render: () => h(App, props) })
+        const vueApp = createApp({ render: () => h(App, props) })
             .use(plugin)
             .mount(el);
+            
+        NProgress.configure({ showSpinner: false });
+        ['start', 'finish', 'error'].forEach(event =>
+            Inertia.on(event, () => NProgress[event === 'start' ? 'start' : 'done']())
+        );
+
+        return vueApp;
     },
 });
